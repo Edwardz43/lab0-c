38ab4553d4edf8c678da42a2cdeae3d8f17b4c42
EdLo <b93303015@gmail.com>
2021-02-28 14:39:17 +0800

    Implement method of inserting new element to head
    Include error handling while allocating failed. Assigning queue tail
    when first time insertion.

--- queue.c
+++ queue.c
@@ -43,13 +43,26 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
+    if (!q)
+        return false;
     list_ele_t *newh;
-    /* TODO: What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
-    /* Don't forget to allocate space for the string and copy it */
-    /* What if either call to malloc returns NULL? */
+    if (!newh)
+        return false;
+    newh->value = malloc(sizeof(char) * strlen(s) + 1);
+    if (!newh->value) {
+        free(newh->value);
+        free(newh);
+        return false;
+    }
+    memset(newh->value, 0, strlen(s) + 1);
+    strncpy(newh->value, s, strlen(s));
+    if (q->size == 0)
+        q->tail = newh;
+
     newh->next = q->head;
     q->head = newh;
+    q->size++;
     return true;
 }
 
