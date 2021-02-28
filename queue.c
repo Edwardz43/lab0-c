a15124d84c14a8a498f8177a0930a8b664dfa02c
EdLo <b93303015@gmail.com>
2021-02-28 12:52:03 +0800

    Implement method of creating new queue
    Initialze after allocating was success and return null if allocating
    failed.

--- queue.c
+++ queue.c
@@ -12,9 +12,9 @@
 queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
-    /* TODO: What if malloc returned NULL? */
-    q->head = NULL;
-    q->tail = 0;
+    if (!q || sizeof(queue_t) == 0)
+        return NULL;
+    q->head = q->tail = NULL;
     q->size = 0;
     return q;
 }
